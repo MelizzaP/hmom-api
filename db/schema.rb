648507e4ab062ad688456c22a44c8e74e12c503b
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-03-30 13:31:43 -0500

    migrate comics
    migration for comics, added title, img_path, and hover_text  to the
    model/schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317171207) do
+ActiveRecord::Schema.define(version: 20150330174728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comics", force: :cascade do |t|
-    t.string   "img_path"
     t.string   "title"
+    t.string   "img_path"
     t.string   "hover_text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
