2b8df2a680fa6cf4cdc02b4a9e875674788df9c1
Melissa Patterson <Melissa.Patterson.va@gmail.com>
2015-04-07 14:28:05 -0500

    add devise for admins
    this sets up the devise routes for admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admins
   root to: 'comics#home'
   resources :comics
   get '/about', to:'comics#about'
   get '/contact', to: 'comics#contact'
-  get '/othercomics', to: 'comics#other_comics' 
+  get '/othercomics', to: 'comics#other_comics'
 end
