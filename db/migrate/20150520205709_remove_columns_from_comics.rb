72cffa8768ef52c3aeb3cbcb396bc79148636302
melissa <mpatterson@opinionlab.com>
2015-05-29 15:34:31 -0500

    fixed migrations
    the migrations were really fucked up so I deleted a couple then added
    the image to comic relation

--- db/migrate/20150520205709_remove_columns_from_comics.rb
+++
@@ -1,8 +0,0 @@
-class RemoveColumnsFromComics < ActiveRecord::Migration
-  def change
-    remove_column :comics, :image_file_name
-    remove_column :comics, :image_content_type
-    remove_column :comics, :image_file_size
-    remove_column :comics, :image_updated_at
-  end
-end
