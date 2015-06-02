0d4b29e4ced39d984d1bb4df996df0c002743f9c
melissa <mpatterson@opinionlab.com>
2015-06-02 16:42:09 -0500

    add some test.... they dont werrrrk

--- spec/controllers/comics_controller_spec.rb
+++ spec/controllers/comics_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ComicsController, type: :controller do
+  describe 'home' do
+    it 'gets the last comic added' do
+      comic = create(:comic)
+      get :home
+      expect(response).to include(comic)
+    end
+  end
 
+  describe 'index' do
+    it 'selects all comics' do
+      create_list(:comic, 5)
+
+    end
+  end
 end
--- spec/factories/comics.rb
+++ spec/factories/comics.rb
@@ -1,8 +1,9 @@
 FactoryGirl.define do
   factory :comic do
-    title "Funny Comic"
-    img_path "../../app/assets/images/comic/alternativeenergy.jpg"
-    hover_text "Brilliant"
+    title 'Funny Comic'
+    hover_text 'Brilliant'
+    image_file_name 'pic.jpg'
+    image_content_type 'image/jpeg'
+    image_file_size 154659
   end
-
 end
