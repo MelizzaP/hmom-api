1ef0b14f4e5f2ef4247e012ed6974d93dc1d4631
melissa <mpatterson@opinionlab.com>
2015-06-05 15:33:19 -0500

    drop migration
    I fucked this up somewhere

--- db/migrate/20150529202050_remove_comic_img_columns.rb
+++
@@ -1,8 +0,0 @@
-class RemoveComicImgColumns < ActiveRecord::Migration
-  def change
-    remove_column :comics, :comic_img_file_name
-    remove_column :comics, :comic_img_content_type
-    remove_column :comics, :comic_img_file_size
-    remove_column :comics, :comic_img_updated_at
-  end
-end
