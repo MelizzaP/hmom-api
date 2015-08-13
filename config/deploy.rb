d090a85bebab3ecba8dfd3ec0b7f67f6b1cd58b0
melissa <mpatterson@opinionlab.com>
2015-08-13 16:51:32 -0500

    precompile assests before deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,14 +18,19 @@ SSHKit.config.command_map[:composer] = "/usr/local/bin/composer"
 set :keep_releases, 5
 
 namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  namespace :assets do
+    task [:precompile, roles: :web] do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
+        run_locally("rake assets:clean && rake assets:precompile")
+        run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+        top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
+        run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
+        run_locally "rm public/assets.tar.bz2"
+        run_locally("rake assets:clean")
+      else
+        logger.info "Skipping asset precompilation because there were no asset changes"
+      end
     end
   end
-
 end
