c4dfd19cdcf8fd9cc6413d2a2292efc9f3af0fb0
melissa <mpatterson@opinionlab.com>
2015-09-10 16:26:50 -0500

    new ip address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
 set :stage, :production
 
-server '104.236.221.116', user: 'mp', roles: %w{app db web}, port: 7853
+server '104.236.19.23', user: 'mp', roles: %w{app db web}, port: 7853
 
-role :app, %w{mp@104.236.221.116}
+role :app, %w{mp@104.236.19.23}
 
 set :bundle_env_variables, { 'NOKOGIRI_USE_SYSTEM_LIBRARIES' => 1 }
 
