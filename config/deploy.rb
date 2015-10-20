06a7002e888051a72a4f915a91df2831504456e8
melissa <mpatterson@opinionlab.com>
2015-10-20 15:37:20 -0500

    I don't fucking know anymore h:-10

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,22 +5,3 @@ set :application, 'hamstermom'
 set :repo_url, 'git@github.com:MelizzaP/hmom-api.git'
 set :branch, 'master'
 set :deploy_to, '/var/www/hmom'
-set :scm, :git
-set :format, :pretty
-set :log_level, :debug
-set :pty, true
-set :user, 'mp'
-set :domain, 'www.hamstermom.com'
-set :port, 7853
-set :linked_files, fetch(:linked_files, [])
-  .push('config/database.yml', 'config/secrets.yml')
-set :linked_dirs, fetch(:linked_dirs, [])
-  .push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :default_env, { path: "/usr/local/bin:$PATH" }
-SSHKit.config.command_map[:composer] = "/usr/local/bin/composer"
-set :keep_releases, 5
-set :sudo_prompt, ""
-set :ssh_options, {forward_agent: true}
-set :local_repository, "ssh://mp@104.236.19.23/home/mp/repositories/hamstermom"
-set :use_sudo, false
-
