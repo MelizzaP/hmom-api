f0c23f6f04eb341dfbd1d83576877065fb7896df
melissa <mpatterson@opinionlab.com>
2015-10-28 16:59:07 -0500

    wip

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,12 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials:  {
+    bucket: ENV['S3_BN'],
+    access_key_id: ENV['AWS_AKI'],
+    secret_access_key: ENV['AWS_SAK']
+  }
+}
 end
