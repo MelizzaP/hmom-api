f6ad8003abcb9b23d4b5ff853c80e9fd6f46f7d6
melissa <mpatterson@opinionlab.com>
2015-11-06 16:49:50 -0600

    add specs for comic controller

--- spec/controllers/comics_controller_spec.rb
+++ spec/controllers/comics_controller_spec.rb
@@ -1,17 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe ComicsController, type: :controller do
-  describe 'home' do
-    xit 'gets the last comic added' do
-      comic = create(:comic)
-      get :home
-      expect(response).to include(comic)
-    end
-  end
+  let(:comic) { create :comic }
+  let(:admin) { create :admin }
 
-  describe 'index' do
-    xit 'selects all comics' do
-      create_list(:comic, 5)
+  context 'renders proper template' do
+    describe 'GET' do
+      it 'home' do
+        get :home
+        expect(response).to render_template(:home)
+      end
+
+      it 'index' do
+        get :index
+        expect(response).to render_template(:index)
+      end
+
+      it 'index' do
+        get :index
+        expect(response).to render_template(:index)
+      end
+
+      it 'about' do
+        get :about
+        expect(response).to render_template(:about)
+      end
+
+      it 'other_comics' do
+        get :other_comics
+        expect(response).to render_template(:other_comics)
+      end
+
+      it 'show' do
+        get :show, { 'id' => comic.id }
+        expect(response).to render_template(:show)
+      end
+
+      context 'admin tasks' do
+        it 'new' do
+          sign_in admin
+          get :new
+          expect(response).to render_template(:new)
+        end
+
+        it 'edit' do
+          sign_in admin
+          get :edit, { id: comic.id }
+          expect(response).to render_template(:edit)
+        end
+      end
     end
   end
 end
