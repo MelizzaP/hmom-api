f6059f83f2edd8439bcec045d2c1b484e9649163
melissa <mpatterson@opinionlab.com>
2015-11-13 14:16:16 -0600

    add mousetrap gem and setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
+//= require mousetrap
 //= require turbolinks
-//= require bootstrap-sprockets
 //= require_tree .
 
