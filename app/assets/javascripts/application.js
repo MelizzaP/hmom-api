89d3dc2f5a3eff72c84927234b55b310ff23fc67
Melissa <melpatenator@gmail.com>
2015-11-14 11:08:27 -0600

    re-order asset pipeline to fix jquery error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require mousetrap
 //= require turbolinks
 //= require_tree .
